1ba39eec537e451d883674246f00a20c5987aca6
HLobisileLukhele <n.thwala03@gmail.com>
2022-09-20 20:08:11 -0700

    point to 98

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _puts - prints a string
+ * @str: string to print
+ *
+ * description: prints a string
+ * on success: returns no error
+ */
+void _puts(char *str)
+{int i = 0;
+while (*(str + i) != '\0')
+{putchar(*(str + i));
+i++; }
+putchar(10);
+}
