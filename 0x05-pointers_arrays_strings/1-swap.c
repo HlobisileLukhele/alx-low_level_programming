782647740c6cc3a9affff2e3e2f4fddb407d1975
HLobisileLukhele <n.thwala03@gmail.com>
2022-09-20 17:24:00 -0700

    swaping the value of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * swap_int - swap two integers vlaues
  * @a: first integer
  * @b: second integer
  * return: nothing
  */
-
 void swap_int(int *a, int *b)
 { 
 int tmp = *a;
 *a = *b;
-*b =tmp;   
-}
+*b = tmp;   
+i}
